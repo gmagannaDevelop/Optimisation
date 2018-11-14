b7783a64a98a37bfe9c7991357923640a764626e
gmagannaDevelop <gmaganna.biomed@gmail.com>
2018-11-14 14:21:42 +0100

    La taille des vecteurs est correcte. Je vais commencer à implementer la méthode du simplexe et voir si ça marche.

--- experiment.m
+++ experiment.m
@@ -7,16 +7,22 @@ load('messagedAlice.mat');
 p = min(size(A));
 m = 4 * p;
 
-xp = ones(1, m); % x'
-t  = zeros(1, p);
+t = ones(m,1); % x'
+xp  = zeros(p,1);
 
 % Ici j'ai essaye d'implementer ce que j'ai vu dans les feuilles
 % Le probleme c'est que la matrice compose Am ne peux pas multiplier x
 % car les dimensions ne sont pas compatibles.
 %x = cat(2, xp, t);
-x = [xp, t];
+
+% La j'hesite entre x = [xp, t] et x = [t , xp]
+% Et aussi entre [,] et [;]
+% il pareil que c'est ;
+x = [t; xp];
+
 Im = eye(m);
 Ip = eye(p);
 O  = zeros(p,m);
 Am = [-Im, A; -Im, -A; O, Ip; O, Ip];
 
+y  = [yprime; -yprime; zeros(p,1); ones(p,1)];
